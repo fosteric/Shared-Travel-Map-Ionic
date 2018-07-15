35b96bc934441b20a29a6db1eaf9666b9aed7ddb
Eric Foster <ericf@zipcoders-MacBook-Pro-3.local>
2018-07-15 06:32:54 -0400

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+
+import { OktaAuthModule, OktaCallbackComponent } from '@okta/okta-angular';
+
+const config = {
+  issuer: 'https://dev-270119.oktapreview.com/oauth2/default',
+  redirectUri: 'http://localhost:8100/implicit/callback',
+  clientId: '0oafqd6lpsD4ECG5k0h7'
+}
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
